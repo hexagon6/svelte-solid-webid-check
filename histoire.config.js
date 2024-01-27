dda42797c9ae2f1469402a3fdde57eea84b67359
Hexagon Six <hexagon6@fet.li>
2024-01-27 11:15:41 +0100

    refactor: change histoire config & story setup for new version

--- histoire.config.js
+++ histoire.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'histoire'
 import { HstSvelte } from '@histoire/plugin-svelte'
 
 export default defineConfig({
+  storyMatch: ['src/lib/*.story.svelte'],
   plugins: [HstSvelte()],
 })
--- src/lib/webid-check.story.svelte
+++ src/lib/webid-check.story.svelte
@@ -8,16 +8,16 @@
   export let Hst
 </script>
 
-<Hst.Story title="webid-check.svelte">
-  <Hst.Variant title="no slot">
+<svelte:component this={Hst.Story} title="webid-check.svelte">
+  <svelte:component this={Hst.Variant} title="no slot">
     <WebIdCheck
       placeholder={'https://timbl.inrupt.net/profile/card#me'}
       on:valid-oidc-endpoint={(
         /** @type {{ type: string; detail: any; }} */ e,
       ) => logEvent(e.type, e.detail)}
     />
-  </Hst.Variant>
-  <Hst.Variant title="custom slots">
+  </svelte:component>
+  <svelte:component this={Hst.Variant} title="custom slots">
     <WebIdCheck
       placeholder={'https://solid.fet.li'}
       on:valid-oidc-endpoint={(
@@ -26,5 +26,5 @@
       ><span slot="label">this is the input for the webid</span>
       <span slot="confirm">please confirm your selection</span>
     </WebIdCheck>
-  </Hst.Variant>
-</Hst.Story>
+  </svelte:component>
+</svelte:component>
