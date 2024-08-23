2f36899e47596d05ef6a8838057e395999d18ac8
Hexagon Six <hexagon6@fet.li>
2024-08-23 15:21:16 +0200

    restyle test page and replace histoire

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,68 @@
+<svelte:options runes={true} />
+
 <script>
   import WebIdCheck from '$lib/webid-check.svelte'
 
-  $: validOIDCEndpoint = ''
+  let validOIDCEndpoint = $state('')
+  const setUrl = ({oidcEndpoint}) => {
+    validOIDCEndpoint = oidcEndpoint
+  }
 </script>
 
 <main>
+  <h2>WebIdCheck Component Example</h2>
+  <span>Type in your webId to check whether there is an openid connect endpoint available</span>
+  {#if validOIDCEndpoint}
   <section>
-    <h2>WebIdCheck</h2>
-      Type in your webId to check whether there is an openid connect endpoint
-      available
-      <WebIdCheck
-        placeholder={'https://timbl.inrupt.net/profile/card#me'}
-        on:valid-oidc-endpoint={({ detail: { oidcEndpoint } }) => {
-          validOIDCEndpoint = oidcEndpoint
-        }}>show</WebIdCheck
-      >
-
-    {#if validOIDCEndpoint}
       <p>
         The OpenID Connect Endpoint is at <a href={validOIDCEndpoint}
           >{validOIDCEndpoint}</a
         >
       </p>
-    {/if}
   </section>
+    {/if}
+  <h3>Default Component</h3>
+  <section>
+
+    <WebIdCheck
+    validOIDCEndpoint={setUrl}>
+  </WebIdCheck>
+  </section>
+  <h3>Variant with "placeholder", "confirm" and "label"</h3>
+  <pre>&lt;WebIdCheck
+  placeholder="https://timbl.inrupt.net/profile/card#me"
+  validOIDCEndpoint=&#123;setUrl&#125;
+&gt;
+  &#123;#snippet&#125;confirm()confirm&#123;/snippet&#125;
+  &#123;#snippet&#125;label()&#125;label&#123;/snippet&#125;
+&lt;/WebIdCheck&gt;</pre>
+  <section>
+    <WebIdCheck
+        placeholder="https://timbl.inrupt.net/profile/card#me"
+        validOIDCEndpoint={setUrl}
+        >
+        {#snippet confirm()}confirm{/snippet}
+        {#snippet label()}label{/snippet}
+    </WebIdCheck>
+  </section>
+
+
 </main>
+
+<style>
+  * {
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+
+  section, pre {
+    display: flex;
+    margin: 2em;
+    padding: 1em;
+    box-shadow: lightgray 1px 1px 2em;
+    border-radius: 0.2em;
+  }
+
+  pre {
+    padding: 1em 1em;
+  }
+</style>
